37e82995adecd51c424dc09faf7d42d3943188df
LukeMed <luke.lam1988@gmail.com>
2024-10-08 16:08:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Operadores Aritméticos
+
+let notaDoMercado = 50
+let pessoasParaDividir = 2
+console.log("Operação de divisão : " + notaDoMercado / pessoasParaDividir)
+
+let calculo = 10 % 4
+console.log(calculo)
